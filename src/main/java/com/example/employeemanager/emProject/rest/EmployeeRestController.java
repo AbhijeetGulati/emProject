583e92126060e145364c4ec9d65ba1da5b223ac6
Abhijeet <abhigulati1000@gmail.com>
2023-04-03 23:18:45 +0530

    creating endpoint to get a single employee based on employeeId

--- src/main/java/com/example/employeemanager/emProject/rest/EmployeeRestController.java
+++ src/main/java/com/example/employeemanager/emProject/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.example.employeemanager.emProject.entity.Employee;
 import com.example.employeemanager.emProject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,20 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
        return employeeService.findAll();//making use of methods declared in EmployeeService
+    }
+
+    //to GET single employee based on employee id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        //we will use method we defined in service implementation to get the reqd employee
+        Employee theEmployee=employeeService.findById(employeeId);
+
+        //exception handling
+        if(theEmployee==null){
+            throw new RuntimeException("Employee id not found : "+employeeId);
+        }
+
+        return theEmployee;
     }
 
 }
